619968ddcf3f8413aad39e6a768295661f57e451
LEKMAD <mohamed.lekmad@um5s.net.ma>
2021-01-22 11:47:57 +0100

    added some graphs

--- interface-admin/src/views/HomePage/HomePage.js
+++ interface-admin/src/views/HomePage/HomePage.js
@@ -5,23 +5,23 @@ import image2 from "../../assets/images/illustrations/undraw_hire_te5y.svg";
 import { Link } from "react-router-dom";
 import {  SettingsIcon } from "../components/icons";
 import { AppContext } from "../../context/AppContext";
+import Dashbord from "../components/Dashbord";
+import RAM from "../components/RAM";
+import DateLogs from "../components/DateLogs";
+import Logs from "../components/Logs";
+import CPULogs from "../components/CPULogs";
+import HD from "../components/HD";
 
 const HomePage = () => {
 
   return (
-    <div className="row">
-       <div
-        className='empty  text-center  "col-md-6" '
-        
-      >
-         <div className="empty-icon">
-          <img src={image1} className="h-8 mb-4" alt="" />
-        </div> 
-        <p className="empty-title h3">You will see here Graphs</p>
+    <div className="page">
+    <div className="page-single">
+      <div className="container">
+      <Logs/>
       </div>
-
-     
-    </div>
+      </div>
+      </div>
   );
 };
 
